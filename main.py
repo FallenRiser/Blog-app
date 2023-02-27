2b47e3a2bd4b3cb06228ff31b38737feccca29cc
Shlok Sheth <shethshlok@gmail.com>
2023-02-27 20:28:30 +0530

    going on blog youtube way completely

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from website import create_app
-from flask import Flask,url_for
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-app = create_app()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this key is super dooper se bhi Uper VaLa SecRET'
+
+class test(FlaskForm):
+    name = StringField("Enter your name here", validators=[DataRequired()])
+    submit =  SubmitField("Submit")
+
+
+
+@app.route('/namepage', methods=['GET','POST'])
+def namepage():
+    name = None
+    form = test() 
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('name.html', name= name, form= form)    
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
